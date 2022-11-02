3b2f8e2a330fccaffa98e2e82ac13c0c1052485c
mukhammadjonmuminov <muhammadjonmominov77@gmail.com>
2022-11-02 16:49:03 +0500

    student app created

--- education/urls.py
+++ education/urls.py
@@ -8,6 +8,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('courses.urls')),
+    path('', include('students.urls')),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
